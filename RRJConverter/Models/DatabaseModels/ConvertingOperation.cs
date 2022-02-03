1d53a9d15706ee622d7db5195d15b01293ee9e9c
Самат Гафиятуллин <stalkersamat@mail.ru>
2022-02-03 04:36:31 +0300

    added LocalDB configuration

---
+++ RRJConverter/Models/DatabaseModels/ConvertingOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RRJConverter.Models
+{
+    public class ConvertingOperation
+    {
+        public int Id { get; set; }
+        public string FromCurrency { get; set; }
+        public int FromCurrencyValue { get; set; }
+        public string ToCurrency { get; set; }
+        public int ToCurrencyValue { get; set; }
+        public DateTime DateOfConvertation { get; set; }
+
+    }
+}
