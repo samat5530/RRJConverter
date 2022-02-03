3c38dbd364724fc2a6fb230c5e303ff16f8d1a1d
Самат Гафиятуллин <stalkersamat@mail.ru>
2022-02-03 04:36:31 +0300

    added LocalDB configuration

---
+++ RRJConverter/Models/DatabaseModels/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RRJConverter.Models.DatabaseModels
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<ConvertingOperation> ConvertingOperations { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+  
+    }
+}
