9eb35f21d87566d549fcac3a48ec0698f4757ac3
Самат Гафиятуллин <stalkersamat@mail.ru>
2022-02-18 12:34:38 +0300

    start to create layers

---
+++ RRJConverter.Domain/ICurrencyRatesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRJConverter.Domain
+{
+    interface ICurrencyRatesService
+    {
+        public Task<DomainCurrencyModel> GetListOfValutesAsync();    
+    }
+}
